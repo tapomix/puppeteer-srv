dd64fa7f7aac915612a0ecac8d5abedd976290af
Tapomix <dev@tapomix.me>
2025-10-28 18:03:06 +0100

    [code] validate APP_TOKEN + SERVER_PORT

--- src/config.js
+++ src/config.js
@@ -1,6 +1,25 @@
 // define config variables from .env
+
+const DEFAULT_TOKEN = 'tapomix_puppeteer-srv_dev-token';
+
 export const APP_ENV = process.env.APP_ENV || 'dev';
-export const APP_PORT = process.env.SERVER_PORT || 3000;
-export const APP_TOKEN = process.env.APP_TOKEN || 'tapomix_puppeteer-srv_dev-token';
+
+// APP_PORT must be set (all environments)
+if (!process.env.SERVER_PORT) {
+    console.error('[FATAL] SERVER_PORT must be set !');
+    process.exit(1);
+}
+export const APP_PORT = process.env.SERVER_PORT;
+
+// APP_TOKEN must be set (+changed from default) for production
+if (
+    APP_ENV === 'prod'
+    && (!process.env.APP_TOKEN || process.env.APP_TOKEN === DEFAULT_TOKEN)
+) {
+    console.error('[FATAL] APP_TOKEN must be set !');
+    process.exit(1);
+}
+export const APP_TOKEN = process.env.APP_TOKEN || DEFAULT_TOKEN;
+
 export const CHROME_EXECUTABLE = process.env.CHROME_EXECUTABLE || '/usr/bin/chromium';
-export const KEEP_BROWSER_OPEN = process.env.KEEP_BROWSER_OPEN === 'true';
+export const KEEP_BROWSER_OPEN = process.env.KEEP_BROWSER_OPEN === 'true'; // string compare !
